5d5446c5f3fe2469562dbcdbd1515393d6e3370c
Mahajbeen-Parveen <138984144+Mahajbeen-Parveen@users.noreply.github.com>
2023-07-20 00:57:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const quizData = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Rome"],
+      answer: "Paris"
+    },
+    {
+      question: "What is the largest planet in our solar system?",
+      options: ["Mars", "Jupiter", "Saturn", "Venus"],
+      answer: "Jupiter"
+    },
+    {
+      question: "Which animal is known as the 'King of the Jungle'?",
+      options: ["Lion", "Elephant", "Tiger", "Giraffe"],
+      answer: "Lion"
+    }
+  ];
+  
+  const questionElement = document.querySelector(".question");
+  const optionsElement = document.querySelector(".options");
+  const submitButton = document.getElementById("submit-btn");
+  const resultElement = document.querySelector(".result");
+  
+  let currentQuestion = 0;
+  let score = 0;
+  
+  function loadQuestion() {
+    const currentQuizData = quizData[currentQuestion];
+    questionElement.textContent = currentQuizData.question;
+    optionsElement.innerHTML = '';
+  
+    for (const option of currentQuizData.options) {
+      const radioInput = document.createElement("input");
+      radioInput.setAttribute("type", "radio");
+      radioInput.setAttribute("name", "option");
+      radioInput.setAttribute("value", option);
+  
+      const label = document.createElement("label");
+      label.textContent = option;
+      label.appendChild(radioInput);
+  
+      optionsElement.appendChild(label);
+    }
+  }
+  
+  function checkAnswer() {
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+  
+    if (!selectedOption) {
+      return;
+    }
+  
+    const userAnswer = selectedOption.value;
+    const currentQuizData = quizData[currentQuestion];
+  
+    if (userAnswer === currentQuizData.answer) {
+      score++;
+    }
+  
+    currentQuestion++;
+  
+    if (currentQuestion < quizData.length) {
+      loadQuestion();
+    } else {
+      showResult();
+    }
+  }
+  
+  function showResult() {
+    optionsElement.innerHTML = '';
+    submitButton.style.display = 'none';
+    resultElement.textContent = `You scored ${score} out of ${quizData.length} questions.`;
+  }
+  
+  loadQuestion();
+  submitButton.addEventListener("click", checkAnswer);
+  
